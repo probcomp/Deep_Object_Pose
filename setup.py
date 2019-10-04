09a7ff63036507b98adfcf1a9b1c2d0ab997f385
austin <austin@agarrett-tp>
2019-10-04 15:59:06 -0400

    Add non-ROS API for obtaining DOPE detections from pure image data (untested/likely buggy).

--- setup.py
+++ setup.py
@@ -1,11 +1,40 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# 
+# from distutils.core import setup
+# from catkin_pkg.python_setup import generate_distutils_setup
+# 
+# # fetch values from package.xml
+# setup_args = generate_distutils_setup(
+#     packages=['dope'],
+#     package_dir={'': 'src'})
+# 
+# setup(**setup_args)
+# 
+# import setuptools
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+# NOTE:
+# This file has been configured to package the core inference functionality.
+# Asynchronous/ROS support is available, but currently unpackaged.
+# If we want to operate DOPE online, we need to reconfigure to support the original packaging.
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['dope'],
-    package_dir={'': 'src'})
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(**setup_args)
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="dope",
+    version="0.0.1",
+    install_requires=requirements,
+    author="Jonathan Tremblay and Thang To and Balakumar Sundaralingam and Yu Xiang and Dieter Fox and Stan Birchfield",
+    description="Deep Object Pose Estimation for Semantic Robotic Grasping of Household Objects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages = ['dope'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
